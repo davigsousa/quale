77d421aefae87abb9c038db0f0e49ec70c443cdf
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2020-08-30 11:27:24 -0300

    genres box added on home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,21 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui';
-import { useEffect } from 'react';
+import { Box, Button } from 'reakit';
+import { useEffect, useState } from 'react';
 import PageTemplate from '../templates/Page';
 import style from '../styles';
 import api, { keyString } from '../services/api';
+const lupe = require('../../static/lupe.png');
 
 export default function Home() {
-  // useEffect(() => {
-  //   (async () => {
-  //     const response = await api.get(`/genre/movie/list?${keyString}`);
+  const [genres, setGenres] = useState([]);
 
-  //     console.log('api', response.data);
-  //   })();
-  // }, []);
+  useEffect(() => {
+    (async () => {
+      const response = await api.get(`/genre/movie/list?${keyString}`);
+      setGenres(response.data.genres);
+    })();
+  }, []);
 
   return (
     <PageTemplate>
@@ -20,6 +23,14 @@ export default function Home() {
         Quer saber <i>qualé</i> ou quais são os filmes mais populares de uma
         categoria?
       </h1>
+      <Box sx={style.twoColumn}>
+        <Box sx={style.genreBox}>
+          {genres.map((item) => (
+            <Button sx={style.genre}>{item.name}</Button>
+          ))}
+        </Box>
+        <img sx={style.lupe} src={lupe} alt="Lupa" />
+      </Box>
     </PageTemplate>
   );
 }
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,8 +1,12 @@
-import { SxStyleProp } from 'theme-ui';
+import { SxStyleProp, Flex } from 'theme-ui';
 
 interface IStyles {
   content: SxStyleProp;
   title: SxStyleProp;
+  twoColumn: SxStyleProp;
+  genreBox: SxStyleProp;
+  genre: SxStyleProp;
+  lupe: SxStyleProp;
 }
 
 const styles: IStyles = {
@@ -16,6 +20,33 @@ const styles: IStyles = {
     fontSize: [4, 4, 5],
     fontFamily: 'heading',
     textAlign: ['center', 'start', 'start'],
+  },
+  twoColumn: {
+    display: 'flex',
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: '10px',
+  },
+  genreBox: {
+    width: '60%',
+    height: '100%',
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  genre: {
+    backgroundColor: '#dcdcdc',
+    color: 'black',
+    padding: '10px',
+    borderRadius: '10px',
+    margin: '5px',
+    fontFamily: 'body',
+    fontWeight: 'heading',
+    fontSize: 2,
+    border: 'none',
+  },
+  lupe: {
+    width: '300px',
   },
 };
 
