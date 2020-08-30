0e0d1ce34ed6aff0afdfbc7d9132ca47ec0ca6c2
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2020-08-30 18:20:57 -0300

    plugin manifest added

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,30 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    title: 'Qualé?',
+    description: 'Descubra informações interessantes sobre filmes.',
+    author: 'Davi Sousa',
+    siteUrl: 'https://quale.netlify.app',
+  },
   plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap',
     'gatsby-plugin-theme-ui',
     {
       resolve: 'gatsby-plugin-anchor-links',
       options: {
         offset: -125,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'Qualé?',
+        short_name: 'Qualé?',
+        start_url: '/',
+        background_color: '#ffffff',
+        theme_color: '#093256',
+        display: 'minimal-ui',
+        icon: '/static/logo.png',
       },
     },
   ],
