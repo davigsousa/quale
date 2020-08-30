54b76d32f7259cee44252efca5ec66a366c414b3
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2020-08-29 22:56:54 -0300

    theme initial configuration added

---
+++ src/gatsby-plugin-theme-ui/defaultStyles.ts
@@ -0,0 +1,27 @@
+export default {
+  h1: {
+    variant: 'text.heading',
+    fontSize: 5,
+  },
+  h2: {
+    variant: 'text.heading',
+    fontSize: 4,
+  },
+  h3: {
+    variant: 'text.heading',
+    fontSize: 3,
+  },
+  table: {
+    width: '100%',
+    borderCollapse: 'separate',
+    borderSpacing: 0,
+  },
+  th: {
+    textAlign: 'left',
+    borderBottomStyle: 'solid',
+  },
+  td: {
+    textAlign: 'left',
+    borderBottomStyle: 'solid',
+  },
+};
--- src/gatsby-plugin-theme-ui/index.ts
+++ src/gatsby-plugin-theme-ui/index.ts
@@ -1,6 +1,22 @@
+import styles from './defaultStyles';
+
 export default {
   colors: {
-    text: '#000',
-    background: '#fff',
+    text: '#fff',
+    background: '#041627',
+    primary: '#3ab6c9',
+    secondary: '#0dbc9c',
   },
+  fontSizes: [12, 14, 16, 20, 24, 32, 48, 64],
+  fontWeights: {
+    body: 400,
+    heading: 700,
+    bold: 700,
+  },
+  fonts: {
+    body: 'Roboto, "Helvetica Neue", sans-serif',
+    heading: '"Helvetica Neue", sans-serif',
+    monospace: 'Menlo, monospace',
+  },
+  styles,
 };
